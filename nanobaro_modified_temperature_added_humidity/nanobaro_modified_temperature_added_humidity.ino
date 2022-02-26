49525ef065c1412befd211638e10c3db049de963
roaxth <roaxth@users.noreply.github.com>
2022-02-26 11:10:27 +0100

    Update nanobaro_modified_temperature_added_humidity.ino
    Changed  the comment section at the beginning

--- nanobaro_modified_temperature_added_humidity/nanobaro_modified_temperature_added_humidity.ino
+++ nanobaro_modified_temperature_added_humidity/nanobaro_modified_temperature_added_humidity.ino
@@ -1,6 +1,11 @@
 // from https://github.com/dolabriform/nanobaro_bmp280/blob/master/nanobaro_bmp280.in0
 //
-// Not working for now. I might have an Issue with the bme280
+// The original NanoBaro was for the bmp and is working fine.
+// I have a BME280 and want to utilize the additional capabilities (humidity)
+// This code isn't quite ready and still under development.
+// It is working so far, but still scrumbled with the demo code wich isn`t needed.
+// I've still some code from the demo and the original NanoBaro code include, but commented out.
+// Further develompment or help by volunteers may increase the quality of this code.
 //
 #include <Adafruit_BMP280.h>
 #include <Wire.h>
